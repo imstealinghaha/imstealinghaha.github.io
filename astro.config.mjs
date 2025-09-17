321665ecc774fff8aeb25bb88f143a0a96ef6ba2
imstealinghaha <prodbysweden@gmail.com>
2025-09-16 21:59:52 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    site: 'https://haydeng.xyz',
+    site: 'https://imstealinghaha.github.io',
     base: '/imstealinghaha.github.io',
   },
 });
